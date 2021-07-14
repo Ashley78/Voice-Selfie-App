64ab9cdbfa94a1d8378af2f4a2e0b6c15d56f029
Ashley78 <86760458+Ashley78@users.noreply.github.com>
2021-07-14 20:31:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+
+var recognition= new SpeechRecognition;
+
+function start(){
+
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult=function run (event){
+
+    console.log(event);
+   
+    var Content=event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML=Content;
+    console.log(Content);
+    if(Content=="take my selfie"){
+        console.log("taking selfie");
+        speak();
+    }
+
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function ()
+     { 
+         take_snapshot();
+         save();
+     },  5000);
+}
+camera= document.getElementById("camera");
+
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'png',
+    jpeg_quality: 90
+  });
+  
+function take_snapshot(){
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML='<img id="image_selfie" src="'+data_uri+'">';
+        });
+}
+function save(){
+     link=document.getElementById("link");
+     image= document.getElementById("image_selfie").src;
+     link.href=image;
+     link.click();
+}
